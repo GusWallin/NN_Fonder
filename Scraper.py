180fc6ca6bf8fa8a341a97c427c0c8bc977388bf
GusWallin <gustaf.wallin@gmail.com>
2020-09-11 04:28:31 +0200

    2nd commit

--- Scraper.py
+++ Scraper.py
@@ -11,15 +11,56 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.common.exceptions import ElementClickInterceptedException
+from selenium import webdriver
+import functions
+
+# settings
+pd.pandas.set_option('display.max_columns', None)
 
 #global variables
-
 file_path = Path().parent.absolute()
-start_page = 1  # page to start from
+start_page = 25  # page to start from
 csv_file_name = 'Fonder_total.csv'
-# path to webdriver for chrome
 
+# path to webdriver for chrome
 DRIVERPATH = r"C:\Program Files (x86)\chromedriver.exe"
 
 # setup
 driver = webdriver.Chrome(DRIVERPATH)
+driver.get("https://www.nordnet.se/marknaden/fondlistor?sortField=name&sortOrder=asc&selectedTab=overview&page=" + str(start_page))
+
+soup = BeautifulSoup(driver.page_source, 'lxml')
+table = soup.find_all('table')[0]
+tab_data = [[cell.text for cell in row.find_all(
+    ["th", "td"])] for row in table.find_all("tr")]
+
+#global variables
+page_counter = int(start_page)
+total_df = pd.DataFrame()
+
+# while loop runs until all pages have been iterated and saved to the csv.
+while True:
+
+    print('page: ' + str(page_counter))
+
+    merge_df = pd.merge(functions.get_fund_info(driver), functions.get_fund_results(
+        driver), right_index=True, left_index=True)
+
+    total_df = total_df.append(merge_df)
+
+    try:
+        functions.get_next_page(driver)
+        page_counter += 1
+
+    except:
+        print('exception, end of pages')
+        break
+
+
+# print(total_df.info())
+# print(total_df.head())
+
+# save output to a file output
+total_df.to_csv('' + str(file_path) + '\\' + csv_file_name)
+
+driver.quit()
---
+++ functions.py
@@ -0,0 +1,58 @@
+from bs4 import BeautifulSoup
+import requests
+import time
+import math
+import numpy as np
+from pathlib import Path
+import pandas as pd
+from tabulate import tabulate
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.common.exceptions import ElementClickInterceptedException
+from selenium import webdriver
+
+
+def get_next_page(driver):
+    element = WebDriverWait(driver, 5).until(
+        EC.presence_of_element_located((By.LINK_TEXT, 'Nästa')))
+    driver.execute_script("window.scrollTo(0,document.body.scrollHeight);")
+    element.click()
+    time.sleep(2)
+
+
+def get_fund_info(driver):
+    element = WebDriverWait(driver, 5).until(
+        EC.presence_of_element_located((By.ID, "tabs-tab-0")))
+    element.click()
+    time.sleep(1)
+
+    soup = BeautifulSoup(driver.page_source, 'lxml')
+    results_table = soup.find_all('table')[0]
+    results_table_data = [[cell.text for cell in row.find_all(
+        ["th", "td"])] for row in results_table.find_all("tr")]
+
+    results_frame = pd.DataFrame(results_table_data)
+    # print(results_frame.head())
+
+    return results_frame
+# function to get the results page and data for each fund.
+
+
+def get_fund_results(driver):
+
+    element = WebDriverWait(driver, 5).until(
+        EC.presence_of_element_located((By.ID, "tabs-tab-1")))
+    element.click()
+    time.sleep(1)
+
+    soup = BeautifulSoup(driver.page_source, 'lxml')
+    results_table = soup.find_all('table')[0]
+    results_table_data = [[cell.text for cell in row.find_all(
+        ["th", "td"])] for row in results_table.find_all("tr")]
+
+    results_frame = pd.DataFrame(results_table_data)
+    # print(results_frame.head())
+
+    return results_frame
